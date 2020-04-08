50baa143449af89dafa86840cbeff3ae37aaaae1
mdiaz26 <mike.diaz006@gmail.com>
2020-04-08 10:39:40 -0400

    init commit--transactions render

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,25 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  
+  state = {
+    transactions: [],
+    searchBar: ""
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(response => response.json())
+    .then(transactions => this.setState({ transactions }))
+  }
+
   render() {
     return (
       <div>
+        <button onClick={() => console.log(this.state)}>Show State</button>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }
