dbbdc3a50bae0c3861dbacb4b1046ae8d6e90ce9
github username <github email address>
2020-04-08 11:16:49 -0400

    3rd deliv

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -7,6 +7,7 @@ class AccountContainer extends Component {
   state = {
     transactions: [],
     newTrans: {},
+    target: null,
   };
   componentDidMount() {
     fetch(transURL)
@@ -15,11 +16,13 @@ class AccountContainer extends Component {
   }
 
   renderTransaction = () => {
-    return this.state.transactions.map((transaction) => {
-      return (
-        <TransactionsList key={transaction.id} transaction={transaction} />
-      );
-    });
+    if (this.state.target) {
+      return <TransactionsList transaction={this.state.target} />;
+    } else {
+      return this.state.transactions.map((transaction, index) => {
+        return <TransactionsList key={index} transaction={transaction} />;
+      });
+    }
   };
   handleSubmit = (event, newTrans) => {
     event.preventDefault();
@@ -36,10 +39,23 @@ class AccountContainer extends Component {
       body: JSON.stringify(newTrans),
     });
   };
+
+  handleSearch = (event) => {
+    let target = event.target.value;
+    let mapping = this.state.transactions.map((transaction) => {
+      let word = transaction.description.toLowerCase();
+      if (word.includes(target)) {
+        target = transaction;
+        return target;
+      }
+    });
+
+    this.setState({ target }, () => console.log(this.state.target));
+  };
   render() {
     return (
       <div>
-        <Search />
+        <Search handleSearch={this.handleSearch} />
         <AddTransactionForm handleSubmit={this.handleSubmit} />
         {this.renderTransaction()}
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const Search = () => {
+const Search = (props) => {
   return (
-    <div className="ui large fluid icon input">
+    <div className='ui large fluid icon input'>
       <input
-        type="text"
-        placeholder={"Search your Recent Transactions"}
-        onChange={() => {
-          console.log("Searching...");
-        }}
+        type='text'
+        placeholder={'Search your Recent Transactions'}
+        onChange={props.handleSearch}
       />
-      <i className="circular search link icon"></i>
+      <i className='circular search link icon'></i>
     </div>
   );
 };
