2bee35f7b8532984df6327a035d52b8d48bfb995
Sasha <apestruy@gmail.com>
2020-04-08 11:20:06 -0400

    search works

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,42 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allTransactions: [],
+      searchedTransactions: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:6001/transactions")
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({
+          allTransactions: data,
+          searchedTransactions: data,
+        });
+      });
+  }
+
+  handleSearch = (event) => {
+    console.log(event.target.value);
+    let searchedTransactions = this.state.allTransactions.filter((t) =>
+      t.description.includes(event.target.value)
+    );
+    this.setState({
+      searchedTransactions: searchedTransactions,
+    });
+  };
+
   render() {
+    console.log(this.state.searchedTransactions);
     return (
       <div>
-        <Search />
+        <Search handleSearch={this.handleSearch} />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.searchedTransactions} />
       </div>
     );
   }
