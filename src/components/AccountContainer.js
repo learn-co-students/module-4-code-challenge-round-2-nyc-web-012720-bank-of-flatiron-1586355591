f275ea2865d32ab14707217db3a9f236a6305cd5
Natalia Wit <natwit95@gmail.com>
2020-04-08 12:05:00 -0400

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,36 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state= {
+    transactions: [],
+    searchTerm: ""
+  }
+  componentDidMount(){
+    fetch("http://localhost:6001/transactions")
+    .then(resp=> resp.json())
+    .then(transactions=>this.getTransactions(transactions))
+  }
+
+  getTransactions = (transactions) => {
+    this.setState({transactions:transactions})
+  }
+
+  addTransaction = (transaction) => {
+    this.setState((prevState)=> ({...prevState.transactions, transaction}))
+  }
+
+  handleSearch = (event) => {
+    this.setState({searchTerm: event.target.value})
+  }
+
   render() {
+    console.log(this.state)
+    const filterTranscation = this.state.transactions.filter(transaction=> transaction.description.toUpperCase().includes(this.state.searchTerm.toUpperCase()))
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search handleSearch={this.handleSearch} />
+        <AddTransactionForm addTransaction={this.addTransaction}/>
+        <TransactionsList transactions={filterTranscation}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,28 +1,86 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
-  render() {
-    return (
-      <div className="ui segment">
-        <form className="ui form">
-          <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
-            <input
-              type="number"
-              name="amount"
-              placeholder="Amount"
-              step="0.01"
-            />
-          </div>
-          <button className="ui button" type="submit">
-            Add Transaction
-          </button>
-        </form>
-      </div>
-    );
-  }
+	state = {
+		dateInput: "",
+		descriptionInput: "",
+		categoryInput: "",
+		amountInput: "",
+	};
+
+	handleChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	handleSubmit = (event) => {
+    event.preventDefault();
+    
+		const data = {
+      date: this.state.dateInput,
+      description: this.state.descriptionInput,
+      category: this.state.categoryInput,
+      amount: this.state.amountInput
+      };
+
+		fetch("http://localhost:6001/transactions", {
+			method: "POST", // or 'PUT'
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				this.props.addTransaction(data)
+			})
+			.then(this.setState({dateInput: "",
+      descriptionInput: "",
+      categoryInput: "",
+      amountInput: ""}))
+	};
+
+	render() {
+		// console.log(this.state);
+		return (
+			<div onSubmit={this.handleSubmit} className="ui segment">
+				<form className="ui form">
+					<div className="inline fields">
+						<input
+							type="date"
+							name="dateInput"
+							value={this.state.dateInput}
+							onChange={this.handleChange}
+						/>
+						<input
+							type="text"
+							name="descriptionInput"
+							placeholder="Description"
+							value={this.state.descriptionInput}
+							onChange={this.handleChange}
+						/>
+						<input
+							type="text"
+							name="categoryInput"
+							placeholder="Category"
+							value={this.state.categoryInput}
+							onChange={this.handleChange}
+						/>
+						<input
+							type="number"
+							name="amountInput"
+							placeholder="Amount"
+							step="0.01"
+							value={this.state.amountInput}
+							onChange={this.handleChange}
+						/>
+					</div>
+					<button className="ui button" type="submit">
+						Add Transaction
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default AddTransactionForm;
