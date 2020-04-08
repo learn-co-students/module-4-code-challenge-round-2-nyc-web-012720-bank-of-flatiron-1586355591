f46a81766c7fa11a626d739ac3c25ee68f77f837
github username <github email address>
2020-04-08 11:29:49 -0400

    Core Deliverables Done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,38 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const API = 'http://localhost:6001/transactions'
+
 class AccountContainer extends Component {
+  state = {
+    transactions: [], 
+    displayTransactions: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => this.setState({
+      transactions: data,
+      displayTransactions: data
+    }))
+  }
+
+  handleSearchBar = (e) => {
+    const searchTerm = e.target.value
+
+    const queriedTransactions = this.state.transactions.filter(transaction => transaction.description.includes(searchTerm))
+    this.setState({
+      displayTransactions: queriedTransactions
+    })
+  }
+
   render() {
     return (
       <div>
-        <Search />
+        <Search handleSearchBar={this.handleSearchBar}/>
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList displayTransactions={this.state.displayTransactions}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
 
+const postApi = 'http://localhost:6001/transactions'
+
 class AddTransactionForm extends Component {
+  state = {
+    date: '', 
+    description: '', 
+    category: '', 
+    amount: 0
+  }
+
+  handleChange = (e) => {
+    this.setState( {[e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const requestObject = {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state)
+    } 
+
+    fetch(postApi, requestObject)
+    .then(response => response.json())
+    .then(data => console.log('Success', data))
+    .catch(error => console.error('Error:', error))
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={(e) => this.handleSubmit(e)}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={(e) => this.handleChange(e)} value={this.state.date}/>
+            <input type="text" name="description" placeholder="Description" onChange={(e) => this.handleChange(e)} value={this.state.description}/>
+            <input type="text" name="category" placeholder="Category" onChange={(e) => this.handleChange(e)} value={this.state.category}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={(e) => this.handleChange(e)}
+              value={this.state.amount}
             />
           </div>
           <button className="ui button" type="submit">
