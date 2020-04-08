7e02f41dcd82c32527cd9a6ccd653076b9500c11
brindab50 <brindab50@gmail.com>
2020-04-08 12:20:45 -0400

    [stuck]

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    description: "",
+
+    category: "",
+
+    amount: ""
+  }
+
+  handleForm = (e) => {
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value
+      
+    })
+    console.log(this.state)
+  }
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={(e) => this.state.handleSubmit (e, this.state)}className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input onChange={this.handleForm}type="date" name="date" />
+            <input onChange={this.handleForm}type="text" name="description" placeholder="Description" />
+            <input onChange={this.handleForm}type="text" name="category" placeholder="Category" />
             <input
               type="number"
               name="amount"
